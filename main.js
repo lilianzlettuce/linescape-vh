5f42867206b0beec4c23da4fbbef814c49952f42
Lilian Zhao <milanzq309@gmail.com>
2021-04-09 19:23:20 -0700

    jsx

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 'use strict';
 
-class Main extends React.Component {
-    constructor(props) {
-        super(props)
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Main = function (_React$Component) {
+    _inherits(Main, _React$Component);
+
+    function Main(props) {
+        _classCallCheck(this, Main);
+
+        return _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this, props));
     }
 
-    render() {
-        return (
-            <div>
+    _createClass(Main, [{
+        key: "render",
+        value: function render() {
+            return React.createElement("div", null);
+        }
+    }]);
 
-            </div>
-        )
-    }
-}
+    return Main;
+}(React.Component);
 
 function Canvas(props) {
-    return (
-        <div>
-            <svg className="" id={props.id} width="600" height="400">
-                <path stroke={props.color} strokeLinecap="round" strokeWidth={props.strokeWidth} fill="transparent" d="" />
-            </svg>
-        </div>
-    )
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "svg",
+            { className: "", id: props.id, width: "600", height: "400" },
+            React.createElement("path", { stroke: props.color, strokeLinecap: "round", strokeWidth: props.strokeWidth, fill: "transparent", d: "" })
+        )
+    );
 }
 
-const domContainer = document.querySelector('#main')
-ReactDOM.render(<Main />, domContainer)
+var domContainer = document.querySelector('#main');
+ReactDOM.render(React.createElement(Main, null), domContainer);
